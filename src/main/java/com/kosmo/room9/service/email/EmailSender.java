e49b3e7611cd437f0730bbd145a1e63d57308d8b
윤홍훈 <harpuria87@gmail.com>
2019-05-24 18:22:17 +0900

    - email sender 수정중...... html 태그 적용 / utf-8 형식으로

--- src/main/java/com/kosmo/room9/service/email/EmailSender.java
+++ src/main/java/com/kosmo/room9/service/email/EmailSender.java
@@ -1,7 +1,9 @@
 package com.kosmo.room9.service.email;
 
+import javax.mail.BodyPart;
 import javax.mail.MessagingException;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,15 +18,21 @@ public class EmailSender {
     public void SendEmail(JavaMailSender mailSender, Email email) throws Exception {
     	System.out.println("들어오니? : " + mailSender);
         MimeMessage msg = mailSender.createMimeMessage();
+
+        
+        
         try {
+        	msg.setHeader("Content-Type", "text/html; charset=UTF-8");
             msg.setSubject(email.getSubject());
-            msg.setText(email.getContent());
+            msg.setContent(email.getContent(), "text/html");
+            //msg.setText(email.getContent());
             msg.setRecipients(MimeMessage.RecipientType.TO , InternetAddress.parse(email.getReceiver()));
            
         }catch(MessagingException e) {
             System.out.println("MessagingException");
             e.printStackTrace();
         }
+        
         try {
             mailSender.send(msg);
         }catch(MailException e) {
